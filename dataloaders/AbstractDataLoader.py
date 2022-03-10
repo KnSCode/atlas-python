672c20b6b01ff3a3abed88fbd0730faf90266482
g0bel1n <lucas.saban@icloud.com>
2022-03-10 13:36:49 +0100

    test

--- dataloaders/AbstractDataLoader.py
+++ dataloaders/AbstractDataLoader.py
@@ -1,4 +1,11 @@
-class AbstractDataLoader():
+from abc import ABC, abstractmethod
+
+class AbstractDataLoader(ABC):
     def __init__(self) -> None:
         self.label_type
+
+    @abstractmethod
+    def get(self,id):
+        return True
+
         
